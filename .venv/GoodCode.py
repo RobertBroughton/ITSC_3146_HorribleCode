a3a79e097140a469dcf1abe757b96ce344433ea4
Robert <bobsaidhi01@gmail.com>
2024-09-24 17:32:17 -0400

    added basic calculator functions & main

---
+++ .venv/GoodCode.py
@@ -0,0 +1,26 @@
+def add(x, y):
+    return x + y
+
+def subtract(x, y):
+    return x - y
+
+def multiply(x, y):
+    return x * y
+
+def divide(x, y):
+    return x / y
+
+x = input("Enter First Number: ")
+y = input("Enter Second Number: ")
+user_input = input("Which Operation do you wish to perform?\n1: Addition\n2: Subtraction\n3: Multiplication\n4: Division\n")
+
+if user_input == "1":
+    print("Answer is: ", add(int(x), int(y)))
+elif user_input == "2":
+    print("Answer is: ", subtract(int(x), int(y)))
+elif user_input == "3":
+    print("Answer is: ", multiply(int(x), int(y)))
+elif user_input == "4":
+    print("Answer is: ", divide(int(x), int(y)))
+else:
+    print("Invalid Input")
